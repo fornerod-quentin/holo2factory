53fc8ae3c24f91e24a1d3db4bf39d86168ee2662
kent-1475 <quentin.fornerod@hotmail.com>
2022-08-08 12:02:08 +0200

    Completed Holo2factoryConfigurator repo

---
+++ holo2factoryConfigurator/MainWindow.xaml.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Newtonsoft.Json;
+namespace Holo2factoryConfigurator
+{
+    public partial class MainWindow : Window
+    {
+        private List<PieceCotation> CotationList { get; set; } = new List<PieceCotation>();
+        private string ModelPath { get; set; }
+        private bool IsSaved { get; set; } = false;
+        private string CurrentDirectory { get; set; } = Directory.GetCurrentDirectory();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddCotation();
+        }
+
+        private void AddCotation()
+        {
+            ProductionWindow WindowProduction = new ProductionWindow(ModelPath);
+            PieceCotation newCotation = new PieceCotation
+            {
+                Id = CotationList.Count()
+            };
+            if (WindowProduction.Execute(newCotation))
+            {
+                CotationList.Add(newCotation);
+                RefreshListBox();
+                IsSaved = false;
+            }
+        }
+        private void RefreshListBox()
+        {
+            Production_ListBox.Items.Clear();
+            foreach (PieceCotation cotation in CotationList)
+            {
+                string dumpedJson = JsonConvert.SerializeObject(cotation);
+                dumpedJson = dumpedJson.Replace('{', ' ').Replace('}', ' ').Replace('"', ' ').Replace(',', '\n');
+                Production_ListBox.Items.Add(dumpedJson);
+            }
+            if (Production_ListBox.Items.Count > 0)
+            {
+                ButtonSave2.IsEnabled = true;
+            }
+            else
+            {
+                ButtonSave2.IsEnabled = false;
+            }
+        }
+
+        private void ButtonModify_Click(object sender, RoutedEventArgs e)
+        {
+            ModifyCotation();
+        }
+
+        private void ModifyCotation()
+        {
+            ProductionWindow WindowProduction = new ProductionWindow(ModelPath);
+            PieceCotation selCotation = CotationList.ElementAt(Production_ListBox.SelectedIndex);
+            if (WindowProduction.Execute(selCotation))
+            {
+                RefreshListBox();
+                IsSaved = false;
+            }
+        }
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you really want to delete this cotation ?", "Delete", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                int selIndex = Production_ListBox.SelectedIndex;
+                CotationList.RemoveAt(selIndex);
+                RemapId();
+                RefreshListBox();
+            }
+        }
+
+        private void ButtonSave2_Click(object sender, RoutedEventArgs e)
+        {
+            SavePiece();
+        }
+        // erase actual piece
+        private void ButtonNewJson_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("This will erase the actual recipe, do you want to conitnue ?", "Clear recipe", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                ClearPiece();
+                IsSaved = false;
+            }
+        }
+        private void ButtonLoadJson_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoadJson())
+            {
+                ButtonSave2.IsEnabled = true;
+                IsSaved = true;
+            }
+            else
+            {
+                ButtonSave2.IsEnabled = false;
+            }
+            RefreshListBox();
+        }
+
+        private bool LoadJson()
+        {
+            const int ExtensionLength = 5;
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                FileName = "Load json piece file",
+                DefaultExt = ".json",
+                Filter = "Text documents (.json)|*.json"
+            };
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                try
+                {
+                    string jsonString = File.ReadAllText(dlg.FileName);
+                    CotationList = JsonConvert.DeserializeObject<List<PieceCotation>>(jsonString);
+                }
+                catch (Exception e)
+                {
+                    _ = MessageBox.Show("Unable to load Json file\nDetails: " + e.Message,
+                        "Load Json", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+                string filename = System.IO.Path.GetFileName(dlg.FileName);
+                TextBlockPieceName.Text = filename.Substring(0, filename.Length - ExtensionLength);
+                TextBlockFilename.Text = filename;
+                return true;
+            }
+            return false;
+        }
+
+        private void Production_ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Production_ListBox.SelectedItem != null)
+            {
+                ButtonModify.IsEnabled = true;
+                ButtonDelete.IsEnabled = true;
+                MenuItemModify.IsEnabled = true;
+                MenuItemDelete.IsEnabled = true;
+            }
+            else
+            {
+                ButtonModify.IsEnabled = false;
+                ButtonDelete.IsEnabled = false;
+                MenuItemModify.IsEnabled = false;
+                MenuItemDelete.IsEnabled = false;
+            }
+        }
+        private void RemapId()
+        {
+            foreach (var (cotation, index) in CotationList.Select((value, i) => (value, i)))
+            {
+                cotation.Id = index;
+            }
+        }
+
+        private void MenuItemNew_Click(object sender, RoutedEventArgs e)
+        {
+            ClearPiece();
+        }
+        private void MenuItemSave_Click(object sender, RoutedEventArgs e)
+        {
+            SavePiece();
+        }
+        private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
+        {
+            LoadJson();
+        }
+        private void ButtonNew_Click(object sender, RoutedEventArgs e)
+        {
+            ClearPiece();
+        }
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            LoadJson();
+        }
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+
+            Exit();
+        }
+        private void Exit()
+        {
+            if (!IsSaved)
+            {
+                MessageBoxResult result = MessageBox.Show("Do you really want to exit ? All unsave data will be lost", "Quit", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.Yes)
+                {
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+        private void MenuItemAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddCotation();
+        }
+
+        private void MenuItemModify_Click(object sender, RoutedEventArgs e)
+        {
+            ModifyCotation();
+        }
+
+        private void MenuItemDelete_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteCotation();
+        }
+
+        private void MenuItemAbout_Click(object sender, RoutedEventArgs e)
+        {
+            _ = MessageBox.Show("Holo2factoryConfigurator\n2022, Quentin Fornerod", "About", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void MenuItemSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            SavePiece();
+        }
+        private void WindowMain_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Exit();
+        }
+        private void ClearPiece()
+        {
+            MessageBoxResult result = MessageBox.Show("Do you really want to clear the list", "Clear list", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                CotationList.Clear();
+                RefreshListBox();
+            }
+        }
+        private void DeleteCotation()
+        {
+            MessageBoxResult result = MessageBox.Show("Do you really want to delete this cotation ?", "Delete", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                int selIndex = Production_ListBox.SelectedIndex;
+                CotationList.RemoveAt(selIndex);
+                RemapId();
+                RefreshListBox();
+            }
+        }
+        private void SavePiece()
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog
+            {
+                FileName = "myNewPiece",
+                DefaultExt = ".json",
+                Filter = "json files (.json)|*.json"
+            };
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                try
+                {
+                    string jsonString = JsonConvert.SerializeObject(CotationList);
+                    File.AppendAllText(dlg.FileName, jsonString);
+                    IsSaved = true;
+                }
+                catch (Exception e)
+                {
+                    _ = MessageBox.Show("Unable to save the cotation list\nDetails: " + e.Message, "Save cotation list", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+        private void Load3DModel()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                FileName = "myModel.obj",
+                DefaultExt = ".obj",
+                Filter = "Obj files (.obj)|*.obj"
+            };
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                TextBlockModel.Text = System.IO.Path.GetFileName(dlg.FileName);
+                ModelPath = dlg.FileName;
+            }
+        }
+        private void ButtonLoadModel_Click(object sender, RoutedEventArgs e)
+        {
+            Load3DModel();
+        }
+        private void MenuItemDocumentation_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                string path = CurrentDirectory + "/Ressources/Holo2factoryConfiguratorUserGuide.pdf";
+                Uri pdf = new Uri(path, UriKind.RelativeOrAbsolute);
+                process.StartInfo.FileName = pdf.LocalPath;
+                process.Start();
+                process.WaitForExit();
+            }
+            catch (Exception exc)
+            {
+                _ = MessageBox.Show("Unable to load the documentation, please check your file\nDetails:" + exc.Message, "Documentation error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            SavePiece();
+        }
+    }
+}
---
+++ holo2factoryConfigurator/ProductionWindow.xaml.cs
@@ -0,0 +1,325 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Media3D;
+using System.IO;
+
+namespace Holo2factoryConfigurator
+{
+    public partial class ProductionWindow : Window
+    {
+        private new bool IsInitialized { get; set; } = false;
+        private string DirectoryPath { get; set; } = Directory.GetCurrentDirectory();
+        private const float CaliperPrecision = 0.01f;
+        private static (double, double, double, double, double, double) ModelOffset { get; set; } = default;
+
+
+        public ProductionWindow(string ModelPath)
+        {
+            InitializeComponent();
+            IsInitialized = true;
+            //window culture
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+
+            ObjReader CurrentHelixObjReader = new ObjReader();
+            try
+            {
+                //load the 3D Model with offset if set
+                Model3DGroup Model3DGroup = CurrentHelixObjReader.Read(ModelPath);
+                Transform3DGroup transform3DGroup = new Transform3DGroup();
+                RotateTransform3D rotateTransform3DX =
+                    new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), ModelOffset.Item4));
+                RotateTransform3D rotateTransform3DY =
+                    new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 1, 0), ModelOffset.Item5));
+                RotateTransform3D rotateTransform3DZ =
+                    new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 0, 1), ModelOffset.Item6));
+                transform3DGroup.Children.Add(rotateTransform3DX);
+                transform3DGroup.Children.Add(rotateTransform3DY);
+                transform3DGroup.Children.Add(rotateTransform3DZ);
+                transform3DGroup.Children.Add(new TranslateTransform3D(new Vector3D(
+                    ModelOffset.Item1, ModelOffset.Item2, ModelOffset.Item3)));
+                myModel.Transform = transform3DGroup;
+                myModel.Content = Model3DGroup;
+                //rewrite adjustments values
+                TextBoxAdjX.Text = ModelOffset.Item1.ToString();
+                TextBoxAdjY.Text = ModelOffset.Item2.ToString();
+                TextBoxAdjZ.Text = ModelOffset.Item3.ToString();
+                TextBoxAdjRx.Text = ModelOffset.Item4.ToString();
+                TextBoxAdjRy.Text = ModelOffset.Item5.ToString();
+                TextBoxAdjRz.Text = ModelOffset.Item6.ToString();
+            }
+            catch (Exception)
+            {
+                _ = MessageBox.Show("Unable to load the model, please check your file", "Load 3D model", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+        }
+        public bool Execute(PieceCotation newCotation)
+        {
+            Owner = Application.Current.MainWindow;
+            LabelCotationNb.Content = "Cotation ID: " + newCotation.Id.ToString();
+            TextBoxX.Text = newCotation.X.ToString();
+            TextBoxY.Text = newCotation.Y.ToString();
+            TextBoxZ.Text = newCotation.Z.ToString();
+            TextBoxRx.Text = newCotation.Rx.ToString();
+            TextBoxRy.Text = newCotation.Ry.ToString();
+            TextBoxRz.Text = newCotation.Rz.ToString();
+            TextBoxExpected.Text = newCotation.ExpectedValue.ToString();
+            TextBoxTolerance.Text = newCotation.Tolerance.ToString();
+            ComboboxType.SelectedItem = newCotation.Type.ToString();
+            bool response = ShowDialog() == true;
+            if (response)
+            {
+                if (ComboboxType.Text == "Rectilign")
+                {
+                    newCotation.Type = PieceCotation.CotationType.Rectilign;
+                }
+                else if (ComboboxType.Text == "Diameter")
+                {
+                    newCotation.Type = PieceCotation.CotationType.Diameter;
+                }
+                else
+                {
+                    newCotation.Type = PieceCotation.CotationType.Hole;
+                }
+                // check values
+                try
+                {
+                    newCotation.X = float.Parse(TextBoxX.Text);
+                    newCotation.Y = float.Parse(TextBoxY.Text);
+                    newCotation.Z = float.Parse(TextBoxZ.Text);
+                    newCotation.Rx = float.Parse(TextBoxRx.Text);
+                    newCotation.Ry = float.Parse(TextBoxRy.Text);
+                    newCotation.Rz = float.Parse(TextBoxRz.Text);
+                    newCotation.ExpectedValue = float.Parse(TextBoxExpected.Text);
+                    newCotation.Tolerance = float.Parse(TextBoxTolerance.Text);
+                }
+                catch (FormatException)
+                {
+                    _ = MessageBox.Show("Float format incorrect, check your values", "Format error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            return response;
+        }
+        //IsCancel is set in xaml
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float.Parse(TextBoxX.Text);
+                float.Parse(TextBoxY.Text);
+                float.Parse(TextBoxZ.Text);
+                float.Parse(TextBoxRx.Text);
+                float.Parse(TextBoxRy.Text);
+                float.Parse(TextBoxRz.Text);
+                float expect = float.Parse(TextBoxExpected.Text);
+                float tol = float.Parse(TextBoxTolerance.Text);
+                if (expect <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Expected value has to be greater than 0");
+
+                }
+                else if (tol < CaliperPrecision)
+                {
+                    throw new ArgumentOutOfRangeException("Tolerance is under the precision of the caliper");
+                }
+                else
+                {
+                    DialogResult = true;
+                }
+            }
+            catch (ArgumentOutOfRangeException exc)
+            {
+                _ = MessageBox.Show(exc.Message, "Range error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+            catch (FormatException)
+            {
+                _ = MessageBox.Show("Float format incorrect, check your values", "Format error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                DialogResult = false;
+            }
+        }
+        private void AdjustModel()
+        {
+            if (!IsInitialized)
+                return;
+            const double Ratio = 10; //Helix uses cm instead of mm
+            double x, y, z, rx, ry, rz;
+            try
+            {
+                x = double.Parse(TextBoxAdjX.Text.Replace(',', '.')) / Ratio;
+                y = double.Parse(TextBoxAdjY.Text.Replace(',', '.')) / Ratio;
+                z = double.Parse(TextBoxAdjZ.Text.Replace(',', '.')) / Ratio;
+                rx = double.Parse(TextBoxAdjRx.Text.Replace(',', '.'));
+                ry = double.Parse(TextBoxAdjRy.Text.Replace(',', '.'));
+                rz = double.Parse(TextBoxAdjRz.Text.Replace(',', '.'));
+                ModelOffset = (x, y, z, rx, ry, rz);
+            }
+            catch (FormatException)
+            {
+                return;
+            }
+            //set transformation to the model with the parameters
+            Transform3DGroup transform3DGroup = new Transform3DGroup();
+            RotateTransform3D rotateTransform3DX = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), rx));
+            RotateTransform3D rotateTransform3DY = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 1, 0), ry));
+            RotateTransform3D rotateTransform3DZ = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 0, 1), rz));
+            transform3DGroup.Children.Add(rotateTransform3DX);
+            transform3DGroup.Children.Add(rotateTransform3DY);
+            transform3DGroup.Children.Add(rotateTransform3DZ);
+            transform3DGroup.Children.Add(new TranslateTransform3D(new Vector3D(x, y, z)));
+            //apply the transformation
+            myModel.Transform = transform3DGroup;
+
+        }
+        private void TextBoxAdjX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustModel();
+        }
+
+        private void TextBoxAdjY_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustModel();
+        }
+
+        private void TextBoxAdjZ_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustModel();
+        }
+
+        private void TextBoxAdjRx_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustModel();
+        }
+
+        private void TextBoxAdjRy_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustModel();
+        }
+
+        private void TextBoxAdjRz_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustModel();
+        }
+
+        private void AdjustCotation()
+        {
+            if (!IsInitialized)
+                return;
+            const double Ratio = 10; //Helix uses cm instead of mm
+            const double minScale = 0.5;
+            const double maxScale = 3;
+            ObjReader CurrentHelixObjReader = new ObjReader();
+            Model3DGroup CotationGroup;
+            double x, y, z, rx, ry, rz, length;
+            try
+            {
+                x = double.Parse(TextBoxX.Text.Replace(',', '.')) / Ratio;
+                y = double.Parse(TextBoxY.Text.Replace(',', '.')) / Ratio;
+                z = double.Parse(TextBoxZ.Text.Replace(',', '.')) / Ratio;
+                rx = double.Parse(TextBoxRx.Text.Replace(',', '.'));
+                ry = double.Parse(TextBoxRy.Text.Replace(',', '.'));
+                rz = double.Parse(TextBoxRz.Text.Replace(',', '.'));
+                length = double.Parse(TextBoxExpected.Text.Replace(',', '.')) / Ratio;
+
+            }
+            catch (FormatException)
+            {
+                return;
+            }
+            catch (System.NullReferenceException)
+            {
+                return;
+            }
+            ScaleTransform3D scaleTransform3D;
+            //Instantiate the correct cotation upon selected type
+            if (ComboboxType.Text == "Rectilign")
+            {
+                CotationGroup = CurrentHelixObjReader.Read(DirectoryPath + "/CotationsModels/CotationArrow.obj");
+                double scaleRatio = length < minScale ? minScale : length > maxScale ? maxScale : length;
+                scaleTransform3D = new ScaleTransform3D(scaleRatio, scaleRatio, scaleRatio, x, y, z);
+            }
+            else if (ComboboxType.Text == "Diameter")
+            {
+                CotationGroup = CurrentHelixObjReader.Read(DirectoryPath + "/CotationsModels/CotationDiameter.obj");
+                scaleTransform3D = new ScaleTransform3D(length, length, 1, x, y, z);
+            }
+            else
+            {
+                CotationGroup = CurrentHelixObjReader.Read(DirectoryPath + "/CotationsModels/CotationHole.obj");
+                double scaleRatio = length < minScale ? minScale : length > maxScale ? maxScale : length;
+                scaleTransform3D = new ScaleTransform3D(scaleRatio, scaleRatio, scaleRatio, x, y, z);
+            }
+            //set transformation to the cotation with the parameters upon the cotation type
+            Transform3DGroup transform3DGroup = new Transform3DGroup();
+            RotateTransform3D rotateTransform3DX = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), rx));
+            RotateTransform3D rotateTransform3DY = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 1, 0), ry));
+            RotateTransform3D rotateTransform3DZ = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 0, 1), rz));
+            transform3DGroup.Children.Add(rotateTransform3DX);
+            transform3DGroup.Children.Add(rotateTransform3DY);
+            transform3DGroup.Children.Add(rotateTransform3DZ);
+            transform3DGroup.Children.Add(new TranslateTransform3D(new Vector3D(x, y, z)));
+            transform3DGroup.Children.Add(scaleTransform3D);
+            CotationGroup.Transform = transform3DGroup;
+            cotationModel.Content = CotationGroup;
+        }
+        //display correct cotation symbol upon cotation type
+
+        private void TextBoxX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustCotation();
+        }
+
+        private void TextBoxY_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustCotation();
+        }
+
+        private void TextBoxZ_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustCotation();
+        }
+
+        private void TextBoxRx_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustCotation();
+        }
+
+        private void TextBoxRy_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustCotation();
+        }
+
+        private void TextBoxRz_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustCotation();
+        }
+
+        private void ComboboxType_DropDownClosed(object sender, EventArgs e)
+        {
+            AdjustCotation();
+        }
+
+        private void TextBoxExpected_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AdjustCotation();
+        }
+    }
+}
